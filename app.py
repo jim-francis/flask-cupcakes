9dd70a0688e00a40e76819379006b49b21f13fa8
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-03-14 14:28:44 -0400

    Create get and post routes

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 """Flask app for Cupcakes"""
 
 from flask import Flask, request, jsonify, render_template
-from secrets import secret_key
 from models import db, connect_db, Cupcake
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = secret_key
+app.config['SECRET_KEY'] = "NGE4ev"
 
 connect_db(app)
 
-def serialize_cupcake(cupcake):
-    """Return SQLAlchemy obj to dictionary"""
-    
-    return{
-        "id": cupcake.id,
-        "flavor": cupcake.flavor,
-        "size": cupcake.size,
-        "rating": cupcake.rating,
-        "image": cupcake.image
-    }
-    
+@app.route('/api/cupcakes')
+def list_cupcakes():
+    """Get all cupcakes"""
+    all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=all_cupcakes)
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    """Get a cupcake"""
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcate():
+    """Make a cupcake"""
+    data = request.json
+    fresh_cupcake = Cupcake(
+        flavor=data["flavor"],
+        rating=data["rating"],
+        size=data["size"],
+        image=data["image"] or None)
+    db.session.add(fresh_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=fresh_cupcake.serialize())
+    return (response_json, 201)
