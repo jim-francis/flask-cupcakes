7929170e54d2516f3594c0279c80911b8ff44a0f
Jimmy Francis <jimmyfrancis@protonmail.com>
2022-03-14 09:48:26 -0400

    Create model and connect to database

--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 """Models for Cupcake app."""
+from flask_sqlalchemy import SQLAlchemy
+
+GENERIC_IMAGE = 'https://tinyurl.com/demo-cupcake'
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+    
+class Cupcake(db.Model):
+    """Cupcake model"""
+    
+    __tablename__ = 'cupcakes'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, nullable=False, default=GENERIC_IMAGE)
+    
+    def image_url(self):
+        return self.photo_url or GENERIC_IMAGE
